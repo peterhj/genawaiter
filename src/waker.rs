dfc64201283261cb764438833181571e1892af87
Peter Jin <>
2023-08-31 10:54:32 -0400

    Minor performance optimization of the waker.
    (This implements the suggestion of gammelalf in #38.)

--- src/waker.rs
+++ src/waker.rs
@@ -1,12 +1,17 @@
 use core::{
+    mem::ManuallyDrop,
+    ops::Deref,
     ptr,
     task::{RawWaker, RawWakerVTable, Waker},
 };
 
-pub fn create() -> Waker {
-    // SAFETY: The waker points to a vtable with functions that do nothing. Doing
-    // nothing is memory-safe.
-    unsafe { Waker::from_raw(RAW_WAKER) }
+#[inline]
+pub fn create() -> impl Deref<Target = Waker> {
+    // SAFETY: The waker points to a vtable with functions
+    // that do nothing, including a no-op drop, as well as
+    // a null data pointer. So, there is nothing to do,
+    // and there is nothing to drop.
+    ManuallyDrop::new(unsafe { Waker::from_raw(RAW_WAKER) })
 }
 
 const RAW_WAKER: RawWaker = RawWaker::new(ptr::null(), &VTABLE);
